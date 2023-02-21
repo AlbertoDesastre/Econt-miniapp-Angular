f0e9d14e3f4e1664d4845676b5bc0cf8cfff317b
Alberto_Desastre <armassantanaalberto@gmail.com>
2023-02-21 11:00:20 +0100

    Major components created. They are hollow

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { AddressesComponent } from './components/addresses/addresses.component';
+import { CartPreviewComponent } from './components/cart-preview/cart-preview.component';
+import { InvoicesComponent } from './components/invoices/invoices.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    AddressesComponent,
+    CartPreviewComponent,
+    InvoicesComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule,
