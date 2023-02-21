c42ca11476ac3b1cfd91a7b16c534e3d1934bd4a
Alberto_Desastre <armassantanaalberto@gmail.com>
2023-02-21 13:02:52 +0100

    Add SCSS of Header and main app

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
-
+  buttons: string[] = [
+    'SHOPPING CARD',
+    'TRANSPORTATION AND DELIVERY',
+    'CONFIRMATION',
+    'METHOD OF PAYMENT',
+  ];
 }
