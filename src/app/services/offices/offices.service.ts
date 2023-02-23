4e2c8b1a672f8bf4924cadb14f6b0ba1ea4d799b
Alberto_Desastre <armassantanaalberto@gmail.com>
2023-02-23 10:40:09 +0100

    Add method to getOffices

--- src/app/services/offices/offices.service.ts
+++ src/app/services/offices/offices.service.ts
@@ -1,9 +1,15 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-
+import { Office } from '../../models/office.model';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class OfficesService {
+  private url =
+    'http://demo.econt.com/ee/services/Nomenclatures/NomenclaturesService.GetOffices.json';
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  getFrom(cityId: number) {
+    return this.http.post<Office[]>(this.url, { countryCode: 'BGR', cityId });
+  }
 }
